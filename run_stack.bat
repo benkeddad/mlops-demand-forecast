9963c3e5e22d994401a5d4620d5a6c62b4ca480a
Youcef Benkeddad <youcef.benkeddad.ext@fimer.com>
2026-06-26 17:14:48 +0200

    run_stack.bat

--- run_stack.bat
+++ run_stack.bat
@@ -2,16 +2,17 @@
 echo Starting MLOps Stack...
 
 echo 📦 Starting MLflow...
-start cmd /k "call .venv\Scripts\activate.bat && mlflow server --host 127.0.0.1 --port 5000"
+start cmd /k "call .venv\Scripts\activate.bat && pip install -r requirements.txt && mlflow server --host 127.0.0.1 --port 5000"
 
 echo 🔮 Starting Prefect...
-start cmd /k "call .venv\Scripts\activate.bat && prefect server start --host 127.0.0.1 --port 4200"
+start cmd /k "call .venv\Scripts\activate.bat && pip install -r requirements.txt && prefect server start --host 127.0.0.1 --port 4200"
 
 :: Wait for 3 seconds to let backend initialize
 timeout /t 3 /nobreak > NUL
 
 echo ⚡ Starting FastAPI...
 call .venv\Scripts\activate.bat
+pip install -r requirements.txt
 set MLFLOW_TRACKING_URI=http://127.0.0.1:5000
 set PREFECT_API_URL=http://127.0.0.1:4200/api
 uvicorn main:app --host 127.0.0.1 --port 8000 --reload
